fa3a18a6ad4bc4535500814c4fb1e20d7d2c5e4e
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2021-11-04 11:49:56 +0000

    added if statement to showName function to check username length and reformatted

--- static_webpage/script.js
+++ static_webpage/script.js
@@ -1,6 +1,11 @@
 function showName() {
-    const name = document.getElementById("name");
-    document.getElementById("helloMessage").innerHTML = `Hello, ${name.value}`;
+  let userInput = document.getElementById("username").value;
+  if (document.getElementById("username").value.length === 0) {
+    return (document.getElementById("helloMessage").innerHTML =
+      "Please enter a username");
+  } else {
+    return (document.getElementById(
+      "helloMessage"
+    ).innerHTML = `Hello, ${userInput}`);
+  }
 }
-document.getElementById('button').onclick = showName;
-
